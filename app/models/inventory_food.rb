355701dc7cc96b340cc8e93fd684759b96e15a52
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-05-30 23:02:01 +0700

    Fix lint err

--- app/models/inventory_food.rb
+++ app/models/inventory_food.rb
@@ -1,5 +1,8 @@
 class InventoryFood < ApplicationRecord
-  validates :quantity, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }, allow_nil: true
+  validates :quantity, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0
+  }, allow_nil: true
   belongs_to :inventory
   belongs_to :food
 end
