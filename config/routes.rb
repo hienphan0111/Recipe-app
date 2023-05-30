cf5596b23a79eede47b38286088f0a434561fd59
Stanley <78428936+chepkok3@users.noreply.github.com>
2023-05-30 15:13:25 +0300

    Add display and add new food

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # , :controllers => { :registrations => 'users/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # resources :users do
+  #   resources :posts
+  # end
+  root 'foods#show'
+
+  resources :foods, only: [:index, :show, :create, :destroy]
 end
