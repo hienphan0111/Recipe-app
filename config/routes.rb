e1fa2f74337805de09c81a91e3c8c22c9d596696
Stanley <78428936+chepkok3@users.noreply.github.com>
2023-05-31 18:38:15 +0300

    Add display a toggle button that allows for a recipe to be made

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
-  # , :controllers => { :registrations => 'users/registrations' }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :foods, only: [:index, :show, :new, :create, :destroy]
 
-  # resources :users do
-  #   resources :posts
-  # end
-  root 'foods#show'
+  resources :recipes, only: [:index, :show, :new, :create] do
+    resources :recipe_foods, only: [:new, :create, :destroy]
+  end
 
-  resources :foods, only: [:index, :show, :create, :destroy]
+
+  authenticated :user do
+    root 'recipes#index', as: :authenticated_root
+  end
+
+  unauthenticated :user do
+    root 'recipes#public_recipes', as: :unauthenticated_root
+  end
+
 end
