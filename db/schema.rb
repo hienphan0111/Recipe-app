6005e8c0f2ed5d8472a426d0567e73bfcd82573f
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-05-31 21:44:46 +0700

    Implement login and registration features

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_135726) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_142221) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.integer "measurement_unit"
@@ -23,7 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_135726) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "users_id"
+    t.bigint "users_id"
     t.index ["users_id"], name: "index_inventories_on_users_id"
   end
 
@@ -31,8 +34,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_135726) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "inventories_id"
-    t.integer "foods_id"
+    t.bigint "inventories_id"
+    t.bigint "foods_id"
     t.index ["foods_id"], name: "index_inventory_foods_on_foods_id"
     t.index ["inventories_id"], name: "index_inventory_foods_on_inventories_id"
   end
@@ -41,8 +44,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_135726) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "recipes_id"
-    t.integer "foods_id"
+    t.bigint "recipes_id"
+    t.bigint "foods_id"
     t.index ["foods_id"], name: "index_recipe_foods_on_foods_id"
     t.index ["recipes_id"], name: "index_recipe_foods_on_recipes_id"
   end
@@ -55,14 +58,25 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_135726) do
     t.boolean "is_public"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "users_id"
+    t.bigint "users_id"
     t.index ["users_id"], name: "index_recipes_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "CreateUsers"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "inventories", "users", column: "users_id"
