cf944f4a8c7f4eaece943ae08d44c1040a25f4a7
Joseph David <jodavid444dave@gmail.com>
2023-05-31 17:11:40 +0100

    display inventory details as in the wireframe

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -2,3 +2,9 @@ class Inventory < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
 end
+
+class Inventory < ApplicationRecord
+  belongs_to :user
+  validates :name, presence: true
+  has_many :inventory_foods
+end
